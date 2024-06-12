d948c749629a488ef661d8c8c5134f681f9ac82f
AnSimonyan <anna.simona1976@gmail.com>
2024-06-12 12:19:13 +0300

    Добавьте файлы проекта.

---
+++ SudokuValide/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SudokuValide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] sudokuArr = new int[9, 9];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    myArr[i, j] = ran.Next(1, 15);
+                    Console.Write("{0}\t", myArr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
