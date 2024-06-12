09b327abcf9856b3c99d968d31e5b592a2e310d8
AnSimonyan <anna.simona1976@gmail.com>
2024-06-12 18:29:14 +0300

    First version

--- SudokuValide/Program.cs
+++ SudokuValide/Program.cs
@@ -4,17 +4,66 @@
     {
         static void Main(string[] args)
         {
-            int[,] sudokuArr = new int[9, 9];
-            for (int i = 0; i < 4; i++)
+            string stringStart = "[[\"5\",\"3\",\".\",\".\",\"7\",\".\",\".\",\".\",\".\"]\r\n,[\"6\",\".\",\".\",\"1\",\"9\",\"5\",\".\",\".\",\".\"]\r\n,[\".\",\"9\",\"8\",\".\",\".\",\".\",\".\",\"6\",\".\"]\r\n,[\"8\",\".\",\".\",\".\",\"6\",\".\",\".\",\".\",\"3\"]\r\n,[\"4\",\".\",\".\",\"8\",\".\",\"3\",\".\",\".\",\"1\"]\r\n,[\"7\",\".\",\".\",\".\",\"2\",\".\",\".\",\".\",\"6\"]\r\n,[\".\",\"6\",\".\",\".\",\".\",\".\",\"2\",\"8\",\".\"]\r\n,[\".\",\".\",\".\",\"4\",\"1\",\"9\",\".\",\".\",\"5\"]\r\n,[\".\",\".\",\".\",\".\",\"8\",\".\",\".\",\"7\",\"9\"]]\r\n";
+
+            int[,] sudokuArr = splitingTheString(stringStart);
+
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 5; j++)
+               
+                for (int j = 0; j < 10; j++)
                 {
-                    myArr[i, j] = ran.Next(1, 15);
-                    Console.Write("{0}\t", myArr[i, j]);
+                   bool resultRow = istheNumberUnique(sudokuArr[0,0],sudokuArr);
+
+
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("Hello, World!");
+         
+        }
+
+        static int[,] splitingTheString(string stringStart)
+        {
+            int[,] sudokuArr = new int[9, 9];
+            char[] separators = new char[] { '\n' };
+            string[] rowsEntered = stringStart.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int i = 0; int j = 0;
+            foreach (string row in rowsEntered)
+            {
+                char[] separatorsRow = new char[] { '\"' };
+                string[] elementsInRow = row.Split(separatorsRow, StringSplitOptions.RemoveEmptyEntries);
+                int k = 0;
+                for (j = 1; j < 18; j = j + 2)
+                {
+                    if (int.TryParse(elementsInRow[j], out int number) == true)
+                    {
+                        int valueInNumber = Convert.ToInt16(elementsInRow[j]);
+                        sudokuArr[i, k] = valueInNumber;
+                    }
+                    else
+                    {
+                        sudokuArr[i, k] = 0;
+                    }
+                    k++;
+                }
+                i = i + 1;
+
+            }
+            return sudokuArr;
+        }
+
+        static bool istheNumberUnique(int valueToSearch, int[] arrayToSearch)
+        {
+            bool resultToReturn = true;
+            int numberOfOccurrences = 0;
+            foreach (int value in arrayToSearch)
+            {
+                if (value == valueToSearch)
+                {
+                    numberOfOccurrences++;
+                }
+            }
+            return (numberOfOccurrences == 1);
         }
     }
 }
