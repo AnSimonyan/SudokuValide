febfc1621dcc64cca714280f168c5db9a49c5bc2
AnSimonyan <anna.simona1976@gmail.com>
2024-06-17 11:22:12 +0300

    Optimization of the string split function

--- SudokuValide/Program.cs
+++ SudokuValide/Program.cs
@@ -4,92 +4,13 @@
     {
         static void Main(string[] args)
         {
-            string stringStart = "[[\"5\",\"3\",\"6\",\".\",\"7\",\".\",\".\",\".\",\".\"]\r\n,[\".\",\".\",\".\",\"1\",\"9\",\"5\",\".\",\".\",\".\"]\r\n,[\".\",\"9\",\"8\",\".\",\".\",\".\",\".\",\"6\",\".\"]\r\n,[\"8\",\".\",\".\",\".\",\"6\",\".\",\".\",\".\",\"3\"]\r\n,[\"4\",\".\",\".\",\"8\",\".\",\"3\",\".\",\".\",\"1\"]\r\n,[\"7\",\".\",\".\",\".\",\"2\",\".\",\".\",\".\",\"6\"]\r\n,[\".\",\"6\",\".\",\".\",\".\",\".\",\"2\",\"8\",\".\"]\r\n,[\".\",\".\",\".\",\"4\",\"1\",\"9\",\".\",\".\",\"5\"]\r\n,[\".\",\".\",\".\",\".\",\"8\",\".\",\".\",\"7\",\"9\"]]\r\n";
+            string stringStart = "[[\"5\",\"3\",\"6\",\".\",\"7\",\".\",\".\",\".\",\".\"]\r\n,[\".\",\".\",\".\",\"1\",\"7\",\"5\",\".\",\".\",\".\"]\r\n,[\".\",\"9\",\"8\",\".\",\".\",\".\",\".\",\"6\",\".\"]\r\n,[\"8\",\".\",\".\",\".\",\"6\",\".\",\".\",\".\",\"3\"]\r\n,[\"4\",\".\",\".\",\"8\",\".\",\"3\",\".\",\".\",\"1\"]\r\n,[\"7\",\".\",\".\",\".\",\"2\",\".\",\".\",\".\",\"6\"]\r\n,[\".\",\"6\",\".\",\".\",\".\",\".\",\"2\",\"8\",\".\"]\r\n,[\".\",\".\",\".\",\"4\",\"1\",\"9\",\".\",\".\",\"5\"]\r\n,[\".\",\".\",\".\",\".\",\"8\",\".\",\".\",\"7\",\"9\"]]\r\n";
             //string stringStart = "[[\"8\",\"3\",\".\",\".\",\"7\",\".\",\".\",\".\",\".\"]\r\n,[\"6\",\".\",\".\",\"1\",\"9\",\"5\",\".\",\".\",\".\"]\r\n,[\".\",\"9\",\"8\",\".\",\".\",\".\",\".\",\"6\",\".\"]\r\n,[\"8\",\".\",\".\",\".\",\"6\",\".\",\".\",\".\",\"3\"]\r\n,[\"4\",\".\",\".\",\"8\",\".\",\"3\",\".\",\".\",\"1\"]\r\n,[\"7\",\".\",\".\",\".\",\"2\",\".\",\".\",\".\",\"6\"]\r\n,[\".\",\"6\",\".\",\".\",\".\",\".\",\"2\",\"8\",\".\"]\r\n,[\".\",\".\",\".\",\"4\",\"1\",\"9\",\".\",\".\",\"5\"]\r\n,[\".\",\".\",\".\",\".\",\"8\",\".\",\".\",\"7\",\"9\"]]\r\n";
-            int[,] sudokuArr = splitingTheString(stringStart); 
-            bool result = true;
+           
+            SudokuValide SudokuValidator = new SudokuValide();
+            int[,] sudokuArr = SudokuValidator.splitingTheString(stringStart);
 
-            for (int i = 0; i < 9; i++)
-            {                             
-                int[] colums = new int[9];//Array for chechink columns
-                for (int k = 0; k < 9; k++) {colums[k] = sudokuArr[k, i]; }
-                
-                int[] row = new int[9];//Array for checking rows
-                for (int k = 0; k < 9; k++) { row[k] = sudokuArr[i, k]; }//Checking columns
-
-                for (int j = 0; j < 9; j++)//Checking each element in sudokuArr by Rows, Columns and Square
-                {
-                    result = istheNumberUnique(sudokuArr[i, j], row);//Checking Row
-                    if (result) { result = istheNumberUnique(sudokuArr[i, j], colums); }//If there is an error we can stop
-
-                    if (result)
-                    {
-                        // Array for checking square
-                        int[] square = new int[9]; int indexSquare = 0; 
-                        int squareLenght = 3; int startSquare = (j / squareLenght)*squareLenght;
-                        for (int k = startSquare; k < startSquare + squareLenght; k++)
-                        {
-                            for (int l = startSquare; l < startSquare + squareLenght; l++)
-                            {
-                                square[indexSquare] = sudokuArr[k, l];
-                                indexSquare++;
-                            }
-                        }
-                        result = istheNumberUnique(sudokuArr[i, j], square);
-                        if (!result){ break; }//If there is an error we can stop
-                    }
-                }
-            }
-            Console.WriteLine(result);
-        }
-
-        static int[,] splitingTheString(string stringStart) //SPLITTING THE ENTERED STRING 
-        {
-            int[,] sudokuArr = new int[9, 9];
-            char[] separators = new char[] { '\n' }; //Splitting the string into rows
-            string[] rowsEntered = stringStart.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            int i = 0; int j = 0;
-            foreach (string row in rowsEntered) //Spitting each row into array[,]
-            {
-                char[] separatorsRow = new char[] { '\"' };
-                string[] elementsInRow = row.Split(separatorsRow, StringSplitOptions.RemoveEmptyEntries);
-                int k = 0;
-                for (j = 1; j < 18; j = j + 2)
-                {
-                    if (int.TryParse(elementsInRow[j], out int number) == true)
-                    {
-                        int valueInNumber = Convert.ToInt16(elementsInRow[j]);
-
-                        sudokuArr[i, k] = valueInNumber;
-                    }
-                    else
-                    {
-                        sudokuArr[i, k] = 0;
-                    }
-                    k++;
-                }
-                i = i + 1;
-
-            }
-            return sudokuArr; 
-        }
-
-        static bool istheNumberUnique(int valueToSearch, int[] arrayToSearch) //Verifying if number is unique in the array []
-        {
-            bool resultToReturn = true;
-            int numberOfOccurrences = 0;
-            foreach (int value in arrayToSearch)
-            {
-                if (value == valueToSearch)
-                {
-                    numberOfOccurrences++;
-                }
-            }
-            if (numberOfOccurrences > 1 && valueToSearch != 0)
-            {
-                resultToReturn = false;
-            }
-            return resultToReturn;
+            Console.WriteLine(SudokuValidator.checkingSudokuArray(sudokuArr));
         }
     }
 }
---
+++ SudokuValide/SudokuValide.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuValide
+{
+
+    internal class SudokuValide
+    {
+        public const int SIZE_SUDOKU = 9;
+        public const int SQUARE_LENGHT = 3;
+
+        public bool checkingSudokuArray(int[,] sudokuArr)
+        {
+            bool result = true;
+
+            for (int i = 0; i < SIZE_SUDOKU; i++)
+            {
+                int[] row       = getCurrentRow (sudokuArr, i);
+                               
+                for (int j = 0; j < SIZE_SUDOKU; j++)//Checking each element in sudokuArr by Row, Column and Square
+                {
+                    if (!istheNumberUnique(sudokuArr[i, j], row)) return false;
+
+                    int[] column = getCurrentColumn(sudokuArr, j);
+                    if (!istheNumberUnique(sudokuArr[i, j], column)) return false; 
+
+                    int[] square = getCurrentSquare(sudokuArr, j);
+                    if (istheNumberUnique(sudokuArr[i, j], square)) return false;
+                }
+            }
+            return result;
+        }
+
+        public int[,] splitingTheString(string stringStart) //Splitting entered string 
+        {
+            int[,] sudokuArr = new int[SIZE_SUDOKU, SIZE_SUDOKU];
+            char[] separators = new char[] { '\r' }; //Splitting the string into rows
+
+            stringStart = clearStringOfExtraCharacters(stringStart); //Clear the string of extra characters
+
+            string[] rowsEntered = stringStart.Split(separators, StringSplitOptions.RemoveEmptyEntries);// In rowsEntered we have an array of each row sudoku
+            int i = 0; 
+            foreach (string row in rowsEntered) //Splitting each row into array[,]
+            {
+                char[] separatorsRow = new char[] { '\"' };
+                string[] elementsInRow = row.Split(separatorsRow, StringSplitOptions.RemoveEmptyEntries);
+                
+                for (int j = 0; j < SIZE_SUDOKU ; j++)
+                {
+                    sudokuArr[i, j] = 0;
+                    if (int.TryParse(elementsInRow[j], out int valueInNumber) == true) sudokuArr[i, j] = valueInNumber;
+                }
+                i = i + 1;
+            }
+            return sudokuArr;
+        }
+
+        private string clearStringOfExtraCharacters(string stringStart)
+        {
+            stringStart = stringStart.Replace("[", string.Empty);
+            stringStart = stringStart.Replace("]", string.Empty);
+            stringStart = stringStart.Replace(",", string.Empty);
+            stringStart = stringStart.Replace("\n", string.Empty);
+            return stringStart;
+        }
+
+        /// <summary>
+        /// test test test
+        /// </summary>
+        /// <param name="sudokuArr"></param>
+        /// <param name="columnNumber"></param>
+        /// <returns></returns>
+        public int[] getCurrentColumn(int[,] sudokuArr, int columnNumber)
+        {
+            int[] column = new int[SIZE_SUDOKU];
+            for (int i = 0; i < SIZE_SUDOKU; i++) column[i] = sudokuArr[ i, columnNumber];
+            return column;
+        }
+
+        public int[] getCurrentRow(int[,] sudokuArr, int rowNumber)
+        {
+            int[] row = new int[SIZE_SUDOKU];
+            for (int k = 0; k < SIZE_SUDOKU; k++) row[k] = sudokuArr[rowNumber, k];
+            return row;
+        }
+
+        public int[] getCurrentSquare(int[,] sudokuArr, int columnNumber)
+        {
+            // Array for checking square
+            int[] square = new int[SIZE_SUDOKU]; int indexSquare = 0;
+            int startSquare = (columnNumber / SQUARE_LENGHT) * SQUARE_LENGHT;
+            int endSquare = startSquare + SQUARE_LENGHT;
+
+            for (int i = startSquare; i < endSquare; i++)
+            {
+                for (int j = startSquare; j < endSquare; j++)
+                {
+                    square[indexSquare] = sudokuArr[i, j];
+                    indexSquare++;
+                }
+            }
+            return square;
+        }
+
+        public bool istheNumberUnique(int valueToSearch, int[] arrayToSearch) //Verifying if number is unique in the array []
+        {
+            bool resultToReturn = true; int numberOfOccurrences = 0;
+
+            foreach (int value in arrayToSearch)
+            {
+                if (value == valueToSearch) numberOfOccurrences++;
+                
+            }
+            if (numberOfOccurrences > 1 && valueToSearch != 0) resultToReturn = false;
+
+            return resultToReturn;
+        }
+    }
+
+}
