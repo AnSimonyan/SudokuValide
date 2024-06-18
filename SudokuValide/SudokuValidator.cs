ac5f39f6d887614eb2aa3db7002dd2eac14dcaa6
AnSimonyan <anna.simona1976@gmail.com>
2024-06-18 18:03:24 +0300

    Version 2

--- SudokuValide/SudokuValidator.cs
+++ SudokuValide/SudokuValidator.cs
@@ -11,6 +11,7 @@ namespace SudokuValide
     {
         public const int SIZE_SUDOKU = 9;
         public const int SQUARE_LENGHT = 3;
+        private int[,] sudokuArr = new int[SIZE_SUDOKU, SIZE_SUDOKU];
 
         /// <summary>
         /// Checking sudoku array [,] is valide by columns, rows and squares 
@@ -23,17 +24,13 @@ namespace SudokuValide
 
             for (int i = 0; i < SIZE_SUDOKU; i++)
             {
-                int[] row       = getCurrentRow (sudokuArr, i);
-                               
                 for (int j = 0; j < SIZE_SUDOKU; j++)//Checking each element in sudokuArr by Row, Column and Square
                 {
-                    if (!istheNumberUnique(sudokuArr[i, j], row)) return false;
+                    if (!isRowValid(sudokuArr[i, j], i)) return false;
 
-                    int[] column = getCurrentColumn(sudokuArr, j);
-                    if (!istheNumberUnique(sudokuArr[i, j], column)) return false; 
+                    if (!isColumnValid(sudokuArr[i, j], j)) return false;
 
-                    int[] square = getCurrentSquare(sudokuArr, i, j);
-                    if (!istheNumberUnique(sudokuArr[i, j], square)) return false;
+                    if (!isCurrentSquareValid(sudokuArr[i, j], i,j)) return false;
                 }
             }
             return result;
@@ -46,19 +43,19 @@ namespace SudokuValide
         /// <returns> int array [,]</returns>
         public int[,] splitingTheString(string stringStart) //Splitting entered string 
         {
-            int[,] sudokuArr = new int[SIZE_SUDOKU, SIZE_SUDOKU];
+            //int[,] sudokuArr = new int[SIZE_SUDOKU, SIZE_SUDOKU];
             char[] separators = new char[] { '\r' }; //Splitting the string into rows
 
             stringStart = clearStringOfExtraCharacters(stringStart); //Clear of extra characters
 
             string[] rowsEntered = stringStart.Split(separators, StringSplitOptions.RemoveEmptyEntries);// In rowsEntered we have an array of each row sudoku
-           
-            for (int i = 0; i< SIZE_SUDOKU; i++) //Splitting each row into array[,]
+
+            for (int i = 0; i < SIZE_SUDOKU; i++) //Splitting each row into array[,]
             {
                 char[] separatorsRow = new char[] { '\"' };
                 string[] elementsInRow = rowsEntered[i].Split(separatorsRow, StringSplitOptions.RemoveEmptyEntries);
-                
-                for (int j = 0; j < SIZE_SUDOKU ; j++)
+
+                for (int j = 0; j < SIZE_SUDOKU; j++)
                 {
                     sudokuArr[i, j] = 0;
                     if (int.TryParse(elementsInRow[j], out int valueInNumber) == true) sudokuArr[i, j] = valueInNumber;
@@ -66,7 +63,7 @@ namespace SudokuValide
             }
             return sudokuArr;
         }
-        
+
         private string clearStringOfExtraCharacters(string stringStart)
         {
             stringStart = stringStart.Replace("[", string.Empty);
@@ -77,74 +74,73 @@ namespace SudokuValide
         }
 
         /// <summary>
-        /// Return an int array[] of column by column number
+        /// Return true if column is valid
         /// </summary>
-        /// <param name="sudokuArr"></param>
+        /// <param name="currentValue"></param>
         /// <param name="columnNumber"></param>
         /// <returns>int array[]</returns>
-        public int[] getCurrentColumn(int[,] sudokuArr, int columnNumber)
+        public bool isColumnValid(int currentValue, int columnNumber)
         {
-            int[] column = new int[SIZE_SUDOKU];
-            for (int i = 0; i < SIZE_SUDOKU; i++) column[i] = sudokuArr[ i, columnNumber];
-            return column;
+            bool resultToReturn = true; int numberOfOccurrences = 0;
+
+            for (int i = 0; i < SIZE_SUDOKU; i++)
+            {
+                if (sudokuArr[i, columnNumber] == currentValue) numberOfOccurrences++;
+
+            }
+            if (numberOfOccurrences > 1 && currentValue != 0) resultToReturn = false;
+
+            return resultToReturn;
+
         }
 
         /// <summary>
-        /// Return an int array[] of row by row number
+        /// Return true if row is valid
         /// </summary>
-        /// <param name="sudokuArr"></param>
+        /// <param name="currentValue"></param>
         /// <param name="rowNumber"></param>
         /// <returns> int array[] </returns>
-        public int[] getCurrentRow(int[,] sudokuArr, int rowNumber)
+        public bool isRowValid(int currentValue, int rowNumber)
         {
-            int[] row = new int[SIZE_SUDOKU];
-            for (int k = 0; k < SIZE_SUDOKU; k++) row[k] = sudokuArr[rowNumber, k];
-            return row;
+            bool resultToReturn = true; int numberOfOccurrences = 0;
+
+            for (int i = 0; i < SIZE_SUDOKU; i++)
+            {
+                if (sudokuArr[rowNumber, i] == currentValue) numberOfOccurrences++;
+
+            }
+            if (numberOfOccurrences > 1 && currentValue != 0) resultToReturn = false;
+
+            return resultToReturn;
         }
 
         /// <summary>
-        /// Return an int array[] of square by column & row numbers
+        /// Return true if square by column & row is valid
         /// </summary>
-        /// <param name="sudokuArr"></param>
+        /// <param name="currentValue"></param>
         /// <param name="rowNumber"></param>
         /// <param name="columnNumber"></param>
         /// <returns>int array[] </returns>
-        public int[] getCurrentSquare(int[,] sudokuArr, int rowNumber, int columnNumber)
+        public bool isCurrentSquareValid(int currentValue,int rowNumber, int columnNumber)
         {
             // Array for checking square
-            int[] square = new int[SIZE_SUDOKU]; int indexSquare = 0;
-            int startSquareRow      = (rowNumber / SQUARE_LENGHT) * SQUARE_LENGHT;
-            int startSquareColumn   = (columnNumber / SQUARE_LENGHT) * SQUARE_LENGHT;
-            
+            //int[] square = new int[SIZE_SUDOKU];
+            bool resultToReturn = true; int numberOfOccurrences = 0;
+            int startSquareRow = (rowNumber / SQUARE_LENGHT) * SQUARE_LENGHT;
+            int startSquareColumn = (columnNumber / SQUARE_LENGHT) * SQUARE_LENGHT;
+
             for (int i = startSquareRow; i < startSquareRow + SQUARE_LENGHT; i++)
             {
                 for (int j = startSquareColumn; j < startSquareColumn + SQUARE_LENGHT; j++)
                 {
-                    square[indexSquare] = sudokuArr[i, j];
-                    indexSquare++;
+                    if (sudokuArr[i, j] == currentValue) numberOfOccurrences++;
                 }
             }
-            return square;
-        }
-        /// <summary>
-        /// Returns true if the number is unique in array [], false if not
-        /// </summary>
-        /// <param name="valueToSearch"></param>
-        /// <param name="arrayToSearch"></param>
-        /// <returns> bool true/false </returns>
-        public bool istheNumberUnique(int valueToSearch, int[] arrayToSearch) //Verifying if number is unique in the array []
-        {
-            bool resultToReturn = true; int numberOfOccurrences = 0;
-
-            foreach (int value in arrayToSearch)
-            {
-                if (value == valueToSearch) numberOfOccurrences++;
-                
-            }
-            if (numberOfOccurrences > 1 && valueToSearch != 0) resultToReturn = false;
-
+            if (numberOfOccurrences > 1 && currentValue != 0) resultToReturn = false;
             return resultToReturn;
         }
+
+        
     }
 
 }
